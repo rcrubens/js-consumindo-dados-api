545aaa21b012bd1e445e3f252e631434dd4205c0
Rubens Campos <rcrubens@live.com>
2023-08-22 22:32:49 -0300

    Iniciando estudos sobre consumo de API com JS

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-async function buscaEndereco(cep) {
-    var mensagemErro = document.getElementById('erro');
-    mensagemErro.innerHTML = "";
-    try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        var consultaCEPConvertida = await consultaCEP.json();
-        if (consultaCEPConvertida.erro) {
-            throw Error('CEP não existente!');
-        }
-        var cidade = document.getElementById('cidade');
-        var logradouro = document.getElementById('endereco');
-        var estado = document.getElementById('estado');
+var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error('Esse CEP não existe!')
+        } else
+        console.log(r)})
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamento concluído!'));
 
-        cidade.value = consultaCEPConvertida.localidade;
-        logradouro.value = consultaCEPConvertida.logradouro;
-        estado.value = consultaCEPConvertida.uf;
-
-        console.log(consultaCEPConvertida);
-        return consultaCEPConvertida;
-    } catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
-        console.log(erro);
-    }
-}
-
-var cep = document.getElementById('cep');
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+console.log(consultaCEP);
